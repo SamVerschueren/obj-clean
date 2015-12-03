e328d1d7f9c90b16e2bcd1e75808052dc727eb29
David Porter <dporter@seek.com.au>
2015-12-03 11:11:49 +1100

    Extended cleaning functionality
    - Removing empty strings from arrays
    - Removing empty strings
    - Handling stings, numbers and arrays explictly

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 'use strict';
 var isPlainObject = require('is-plain-object');
 function clean(obj) {
-	return Object.keys(obj).reduce(function (result, key) {
-		if (obj[key] === null || obj[key] === undefined) {
-			return result;
-		}
-
-		if (isPlainObject(obj[key])) {
-			var res = clean(obj[key]);
-
-			if (Object.keys(res).length > 0) {
-				result[key] = res;
+	if (obj && isPlainObject(obj)) {
+		return Object.keys(obj).reduce(function (result, key) {
+			if (obj[key] === null || obj[key] === undefined) {
+				return result;
 			}
-		} else if (obj[key] !== '') {
-			result[key] = obj[key];
-		}
 
-		return result;
-	}, {});
+			if (isPlainObject(obj[key])) {
+				var res = clean(obj[key]);
+
+				if (Object.keys(res).length > 0) {
+					result[key] = res;
+				}
+			} else if (obj[key].constructor === Array) {
+				var cleanedArray = obj[key].map(function (el) {
+					return clean(el);
+				}).filter(function (ea) {
+					return ea !== undefined;
+				});
+				if (cleanedArray.length) {
+					result[key] = cleanedArray;
+				}
+			} else if (obj[key] !== '') {
+				result[key] = obj[key];
+			}
+
+			return result;
+		}, {});
+	} else if (obj.constructor === Array) {
+		return obj.map(function (ea) {
+			return clean(ea);
+		}).filter(function (ea) {
+			return ea !== undefined;
+		});
+	} else if (typeof obj === 'string') {
+		if (obj.length) {
+			return obj;
+		}
+	} else if (typeof obj === 'number' && !isNaN(obj)) {
+		return obj;
+	}
 }
 
 module.exports = clean;
