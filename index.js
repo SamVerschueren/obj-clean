e1d9d5bd1409676c13de06253383da3c19d7c480
Sam Verschueren <sam.verschueren@gmail.com>
2019-05-21 19:40:46 +0200

    Meta tweaks

--- index.js
+++ index.js
@@ -2,21 +2,22 @@
 const isPlainObject = require('is-plain-obj');
 
 const clean = (obj, options) => {
-	options = Object.assign({
-		preserveArrays: true
-	}, options);
+	const opts = {
+		preserveArrays: true,
+		...options
+	};
 
 	return Object.keys(obj).reduce((result, key) => {
 		if (obj[key] === null || obj[key] === undefined) {
 			return result;
 		}
 
-		if (options.preserveArrays === false && Array.isArray(obj[key]) && obj[key].length === 0) {
+		if (opts.preserveArrays === false && Array.isArray(obj[key]) && obj[key].length === 0) {
 			return result;
 		}
 
 		if (isPlainObject(obj[key])) {
-			const res = clean(obj[key], options);
+			const res = clean(obj[key], opts);
 
 			if (Object.keys(res).length > 0) {
 				result[key] = res;
--- test.js
+++ test.js
@@ -1,32 +1,32 @@
 import test from 'ava';
-import fn from './';
+import clean from '.';
 
 test('shallow', t => {
-	t.deepEqual(fn({}), {});
-	t.deepEqual(fn({foo: undefined}), {});
-	t.deepEqual(fn({foo: null}), {});
-	t.deepEqual(fn({foo: 'bar', baz: undefined}), {foo: 'bar'});
-	t.deepEqual(fn({foo: 'bar', baz: null}), {foo: 'bar'});
-	t.deepEqual(fn({foo: 'bar', baz: null, bax: true}), {foo: 'bar', bax: true});
-	t.deepEqual(fn({foo: 'bar', baz: null, bax: false}), {foo: 'bar', bax: false});
-	t.deepEqual(fn({foo: ''}), {});
-	t.deepEqual(fn({foo: 'bar', baz: ''}), {foo: 'bar'});
-	t.deepEqual(fn({foo: 'bar', baz: []}), {foo: 'bar', baz: []});
-	t.deepEqual(fn({foo: 'bar', baz: []}, {preserveArrays: false}), {foo: 'bar'});
+	t.deepEqual(clean({}), {});
+	t.deepEqual(clean({foo: undefined}), {});
+	t.deepEqual(clean({foo: null}), {});
+	t.deepEqual(clean({foo: 'bar', baz: undefined}), {foo: 'bar'});
+	t.deepEqual(clean({foo: 'bar', baz: null}), {foo: 'bar'});
+	t.deepEqual(clean({foo: 'bar', baz: null, bax: true}), {foo: 'bar', bax: true});
+	t.deepEqual(clean({foo: 'bar', baz: null, bax: false}), {foo: 'bar', bax: false});
+	t.deepEqual(clean({foo: ''}), {});
+	t.deepEqual(clean({foo: 'bar', baz: ''}), {foo: 'bar'});
+	t.deepEqual(clean({foo: 'bar', baz: []}), {foo: 'bar', baz: []});
+	t.deepEqual(clean({foo: 'bar', baz: []}, {preserveArrays: false}), {foo: 'bar'});
 });
 
 test('deep', t => {
-	t.deepEqual(fn({foo: {}}), {});
-	t.deepEqual(fn({foo: {bar: undefined}}), {});
-	t.deepEqual(fn({foo: {bar: null}}), {});
-	t.deepEqual(fn({foo: {bar: {}}}), {});
-	t.deepEqual(fn({foo: {bar: 'baz', baz: null}}), {foo: {bar: 'baz'}});
-	t.deepEqual(fn({foo: {bar: 'baz', baz: undefined}}), {foo: {bar: 'baz'}});
-	t.deepEqual(fn({foo: {bar: 'baz', baz: null, bax: true}}), {foo: {bar: 'baz', bax: true}});
-	t.deepEqual(fn({foo: {bar: 'baz', baz: undefined, bax: false}}), {foo: {bar: 'baz', bax: false}});
-	t.deepEqual(fn({foo: {bar: 'baz', baz: ''}}), {foo: {bar: 'baz'}});
-	t.deepEqual(fn({foo: {bar: 'baz', baz: {}}}), {foo: {bar: 'baz'}});
-	t.deepEqual(fn({foo: {bar: 'baz', baz: {bar: 'baz', baz: ''}}}), {foo: {bar: 'baz', baz: {bar: 'baz'}}});
-	t.deepEqual(fn({foo: 'bar', bar: {baz: []}}), {foo: 'bar', bar: {baz: []}});
-	t.deepEqual(fn({foo: 'bar', bar: {baz: []}}, {preserveArrays: false}), {foo: 'bar'});
+	t.deepEqual(clean({foo: {}}), {});
+	t.deepEqual(clean({foo: {bar: undefined}}), {});
+	t.deepEqual(clean({foo: {bar: null}}), {});
+	t.deepEqual(clean({foo: {bar: {}}}), {});
+	t.deepEqual(clean({foo: {bar: 'baz', baz: null}}), {foo: {bar: 'baz'}});
+	t.deepEqual(clean({foo: {bar: 'baz', baz: undefined}}), {foo: {bar: 'baz'}});
+	t.deepEqual(clean({foo: {bar: 'baz', baz: null, bax: true}}), {foo: {bar: 'baz', bax: true}});
+	t.deepEqual(clean({foo: {bar: 'baz', baz: undefined, bax: false}}), {foo: {bar: 'baz', bax: false}});
+	t.deepEqual(clean({foo: {bar: 'baz', baz: ''}}), {foo: {bar: 'baz'}});
+	t.deepEqual(clean({foo: {bar: 'baz', baz: {}}}), {foo: {bar: 'baz'}});
+	t.deepEqual(clean({foo: {bar: 'baz', baz: {bar: 'baz', baz: ''}}}), {foo: {bar: 'baz', baz: {bar: 'baz'}}});
+	t.deepEqual(clean({foo: 'bar', bar: {baz: []}}), {foo: 'bar', bar: {baz: []}});
+	t.deepEqual(clean({foo: 'bar', bar: {baz: []}}, {preserveArrays: false}), {foo: 'bar'});
 });
